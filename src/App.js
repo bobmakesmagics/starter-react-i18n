b5878a6c6de92f9d7c8d9d8b8160cb7f390f5d51
bobmakesmagics <wildhorse1012@gmail.com>
2023-11-28 18:54:50 +0300

    added langusage swicher searchable select dropdown

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import './App.css';
 import { Suspense, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import Select from 'react-select';
 
-const locales = {
-  en: { title: 'English' },
-  es: { title: 'Español' },
-};
+const options = [
+  { value: 'en', label: 'English' },
+  { value: 'es', label: 'Español' },
+];
 
 function App() {
   const { t, i18n } = useTranslation();
   const [messages, setMessages] = useState(0);
 
   return (
-    <div>
-      <ul>
-        {Object.keys(locales).map((locale) => (
-          <li key={locale}>
-            <button
-              style={{
-                fontWeight:
-                  i18n.resolvedLanguage === locale ? 'bold' : 'normal',
-              }}
-              type="submit"
-              onClick={() => i18n.changeLanguage(locale)}
-            >
-              {locales[locale].title}
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div className="App">
+      <Select
+        options={options}
+        defaultValue={options[0]}
+        onChange={(opt) => i18n.changeLanguage(opt.value)}
+      />
       <h1>{t('main.header')}</h1>
       <button onClick={() => setMessages(messages + 1)}>+1 message</button>
       <p>{t('main.new_messages', { count: messages })}</p>
